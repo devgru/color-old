7303cb3171d18ae72a7fcf6af3ee2450f5a6baae
Dima Devgru <git@devg.ru>
2019-04-25 23:14:09 +0300

    Updates

---
+++ precache-manifest.d45287ad65b72ab0697d85320d85bfc9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9ca310d2aa6ac212ed45cb5663a4851",
+    "url": "/color-old/index.html"
+  },
+  {
+    "revision": "a05bdd1ef17583b4c47d",
+    "url": "/color-old/static/css/main.b558bdb2.chunk.css"
+  },
+  {
+    "revision": "5bb8504eb8e9551e6784",
+    "url": "/color-old/static/js/2.aacfd06c.chunk.js"
+  },
+  {
+    "revision": "a05bdd1ef17583b4c47d",
+    "url": "/color-old/static/js/main.1c5cc3b8.chunk.js"
+  },
+  {
+    "revision": "3361d0f7240bd359044b",
+    "url": "/color-old/static/js/runtime~main.8328efba.js"
+  }
+]);
